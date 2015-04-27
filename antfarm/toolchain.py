45d224ad078119df96e531ef7a65e1924e03c082
George Peristerakis <gperiste@redhat.com>
2015-04-27 18:08:43 -0400

    typo

--- antfarm/toolchain.py
+++ antfarm/toolchain.py
@@ -16,7 +16,7 @@ class GraphFeed(object):
     def buildGraph(self, offset=0, limit=100):
         rows = m.session.query(m.Message).offset(offset).limit(limit).all()
         for row in rows:
-            topic = row.topic.translate('_', '.')
+            topic = row.topic.replace('.', '_')
             cypher = 'MATCH (u:user {name:{user}}), (p:package {name: {pkg}}) ' \
                      'MERGE (u)-[r:' + topic + ' {timestamp: {time}, category: {cat}, msg_id: {msg_id}}]->(p) ' \
                      'RETURN r'
